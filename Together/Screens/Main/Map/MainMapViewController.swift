a75033c5c74dfb3d03316a63487762606ac4651b
Tublad <tublad92@gmail.com>
2020-08-06 12:49:09 +0600

    add animation for searchTextField

--- Together/Screens/Main/Map/MainMapViewController.swift
+++ Together/Screens/Main/Map/MainMapViewController.swift
@@ -10,15 +10,30 @@ final class MainMapViewController: UIViewController {
     
     //MARK: - Open properties
     // презентору сообщаем обо всех действиях и передаем данные, например: презентер, была нажата кнопка войти
+    enum SlideState {
+        case expanded
+        case collapsed
+    }
+    
     var presenter: MainMapViewActions?
-    var slideMenuViewController: SlideMenuViewController?
+    var slideMenuViewController: SlideMenuViewController!
     
     //MARK: - Private properties
+    private let slideViewHeight: CGFloat = 600
+    private let slideHandleAreaHeight: CGFloat = 150
+    
+    private var slideViewVisible = false
+    private var nextState: SlideState {
+        return slideViewVisible ? .collapsed : .expanded
+    }
+    
+    private var runningAnimations = [UIViewPropertyAnimator]()
+    private var animationProgressWhenInterrupted: CGFloat = 0
     //вью просим отобразить контент
     private lazy var mainView = view as? (MainMapViewImpl & PresenterHaving)
-    //private var visualEffectView: UIVisualEffectView?
+    private var visualEffectView: UIVisualEffectView?
     
-    private let slideMenuViewControllerHeight: CGFloat = 135
+    private let slideMenuViewControllerHeight: CGFloat = 145
     
     //MARK: - Life cycle
     override func loadView() {
@@ -44,25 +59,100 @@ final class MainMapViewController: UIViewController {
     //Устанавливаем навигацию, заголовок навигейшен контроллера, кнопки на навиг контроллере
     private func showSlideMenu() {
         // to do ..
-//        visualEffectView = UIVisualEffectView()
-//        visualEffectView?.frame = self.view.frame
-//        guard let visualView = visualEffectView else { return }
+        visualEffectView = UIVisualEffectView()
+        visualEffectView?.frame = self.view.bounds
+        visualEffectView?.alpha = 0
+        guard let visualView = visualEffectView else { return }
 
-//        self.view.addSubview(visualView)
-        guard let slideViewController = slideMenuViewController else { return }
-        slideViewController.view.frame = CGRect(x: 0,
+        self.view.addSubview(visualView)
+        
+//        guard let slideViewController = slideMenuViewController else { return }
+        slideMenuViewController.view.frame = CGRect(x: 0,
                                                 y: self.view.frame.height - slideMenuViewControllerHeight ,
-                                                width: self.view.frame.width,
-                                                height: slideMenuViewControllerHeight)
+                                                width: self.view.bounds.width,
+                                                height: slideViewHeight)
         
         //self.addChild(slideViewController)
-        self.view.addSubview(slideViewController.view)
+        self.view.addSubview(slideMenuViewController.view)
+//        slideMenuViewController.view.clipsToBounds = true
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(MainMapViewController.handleSearchTap(recognizer:)))
+        let panGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(MainMapViewController.handleSearchPan(recognizer:)))
+        
+        slideMenuViewController.view.addGestureRecognizer(tapGestureRecognizer)
+        slideMenuViewController.view.addGestureRecognizer(panGestureRecognizer)
     }
     
     private func setNavigation() {
-        navigationController?.navigationBar.isHidden = false
         navigationItem.title = "Главная"
     }
+    
+    @objc func handleSearchTap(recognizer: UITapGestureRecognizer) {
+        
+    }
+    
+    @objc func handleSearchPan(recognizer: UITapGestureRecognizer) {
+        
+        switch recognizer.state {
+        case .began:
+            print("start")
+            startInterectiveTransition(state: nextState, duration: 0.9)
+        case .changed:
+            print("update")
+            updateInterectiveTransition(fractionCompleted: 0)
+        case .ended:
+            print("continue")
+            continueInterectiveTransition()
+        default:
+            break
+        }
+    }
+    
+    private func animateTransitionIfNeeded(state: SlideState, duration: TimeInterval) {
+        if runningAnimations.isEmpty {
+            let frameAnimator = UIViewPropertyAnimator(duration: duration,
+                                                       dampingRatio: 1) {
+                                                        switch state {
+                                                        case .expanded:
+                                                            self.slideMenuViewController.view.frame.origin.y = self.view.frame.height - self.slideViewHeight
+                                                        case .collapsed:
+                                                            self.slideMenuViewController.view.frame.origin.y = self.view.frame.height - self.slideHandleAreaHeight
+                                                        }
+            }
+            
+            frameAnimator.addCompletion { (_) in
+                self.slideViewVisible = !self.slideViewVisible
+                self.runningAnimations.removeAll()
+            }
+            
+            frameAnimator.startAnimation()
+            runningAnimations.append(frameAnimator)
+        }
+    }
+    
+    private func startInterectiveTransition(state: SlideState, duration: TimeInterval) {
+        if runningAnimations.isEmpty {
+            print("run animations")
+            animateTransitionIfNeeded(state: state, duration: duration)
+        }
+        for animator in runningAnimations {
+            animator.pauseAnimation()
+            animationProgressWhenInterrupted = animator.fractionComplete
+        }
+    }
+    
+    private func updateInterectiveTransition(fractionCompleted: CGFloat) {
+        for animator in runningAnimations {
+            animator.fractionComplete = fractionCompleted + animationProgressWhenInterrupted
+        }
+    }
+    
+    private func continueInterectiveTransition() {
+        for animator in runningAnimations {
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        }
+    }
+    
 }
 
 
--- Together/Screens/Main/Map/SlideMenuView/SlideMenuView.swift
+++ Together/Screens/Main/Map/SlideMenuView/SlideMenuView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 protocol SlideMenuViewImpl {
     //функции типа, покажи данные
-    func setPresenter(_ presenter: SlideMenuViewAction)
+    
 }
 
 final class SlideMenuView: UIView {
@@ -18,30 +18,38 @@ final class SlideMenuView: UIView {
     var presenter: SlideMenuViewAction?
     
     //MARK: - Private properties
+    
+    private let slideViewHeight: CGFloat = 300
+    private let slideHandleAreaHeight: CGFloat = 150
+    private var slideVisible = false
+    
+    private var rightAnchorConstraint: NSLayoutConstraint?
+    
     private lazy var lineView: UIView = {
         let view = UIView()
-        view.backgroundColor = .white
+
+        view.backgroundColor = .darkGray
         view.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.layer.cornerRadius = 3
+        view.clipsToBounds = true
         return view
     }()
     
     private lazy var searchTextField: UITextField = {
-        let textField = UITextField()
-        
-        // add image in left part textField
-        let imageView = UIImageView()
-        let image = UIImage(systemName: "magnifyingglass")
-        imageView.image = image
-        imageView.tintColor = UIColor.blueLocationButton
-        textField.leftView = imageView
-        
-        textField.placeholder = "Поиск помощи по категории"
-        textField.tintColor = UIColor.blueLocationButton
-        textField.backgroundColor = UIColor.grayLocationButton
-        
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        
+        let textField = SearchMapTextField()
         return textField
+    }()
+    
+    private lazy var closedButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = UIColor.blueButton
+        button.setTitle("Отменить", for: .normal)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        button.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(closedSlideMenuView)))
+        return button
     }()
     
     //MARK: - Init
@@ -57,32 +65,96 @@ final class SlideMenuView: UIView {
     
     //MARK: - Private metods
     fileprivate func setupUI() {
-        self.backgroundColor = UIColor.white
+        setupSelfView()
         setupLineView()
         setupSearchTextField()
     }
     
+    private func setupSelfView() {
+        self.backgroundColor = UIColor.white
+        self.alpha = 0.9
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+        
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 2
+        self.layer.shadowRadius = 4
+        self.layer.masksToBounds = false
+    }
+    
     private func setupLineView() {
-//        lineView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-//        lineView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-//        lineView.widthAnchor.constraint(equalToConstant: 50).isActive = true
-//        lineView.heightAnchor.constraint(equalToConstant: 5).isActive = true
+        self.addSubview(lineView)
+        
+        lineView.topAnchor.constraint(equalTo: self.topAnchor, constant: 5).isActive = true
+        lineView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        lineView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        lineView.heightAnchor.constraint(equalToConstant: 5).isActive = true
     }
     
     private func setupSearchTextField() {
-//        searchTextField.topAnchor.constraint(equalTo: lineView.bottomAnchor, constant: 5).isActive = true
-//        searchTextField.leftAnchor.constraint(equalTo: self.leftAnchor, constant: sideArchoreConctant).isActive = true
-//        searchTextField.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -sideArchoreConctant).isActive = true
-//        searchTextField.bottomAnchor.constraint(equalToSystemSpacingBelow: self.bottomAnchor, multiplier: 0).isActive = true
-//        searchTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        self.addSubview(searchTextField)
+        
+        searchTextField.topAnchor.constraint(equalTo: lineView.bottomAnchor, constant: 3).isActive = true
+        searchTextField.leftAnchor.constraint(equalTo: self.leftAnchor, constant: sideArchoreConctant).isActive = true
+        
+        rightAnchorConstraint = searchTextField.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -sideArchoreConctant)
+        rightAnchorConstraint?.isActive = true
+        
+        searchTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        searchTextField.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(openSlideMenuView)))
     }
-}
-
-
-extension SlideMenuView: SlideMenuViewImpl {
     
-    func setPresenter(_ presenter: SlideMenuViewAction) {
-        self.presenter = presenter
+    @objc private func openSlideMenuView(){
+            UIView.animate(withDuration: 0.9,
+                           delay: 0,
+                           usingSpringWithDamping: 1,
+                           initialSpringVelocity: 1,
+                           options: .curveEaseOut,
+                           animations: {
+
+                            self.frame.origin.y = self.frame.height - self.slideViewHeight
+                            self.slideVisible = true
+                            self.closedButton.isHidden = false
+                            
+                            self.rightAnchorConstraint?.isActive = false
+                            self.rightAnchorConstraint = self.searchTextField.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -100)
+                            self.rightAnchorConstraint?.isActive = true
+                            
+                            self.addSubview(self.closedButton)
+                            self.closedButton.leftAnchor.constraint(equalTo: self.searchTextField.rightAnchor, constant: 5).isActive = true
+                            self.closedButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -5).isActive = true
+                            self.closedButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 15).isActive = true
+                            
+            }, completion: nil)
+    }
+    
+    @objc private func closedSlideMenuView() {
+            UIView.animate(withDuration: 0.9,
+                           delay: 0,
+                           usingSpringWithDamping: 1,
+                           initialSpringVelocity: 1,
+                           options: .curveEaseOut,
+                           animations: {
+                            
+                            self.frame.origin.y = self.frame.height + self.slideHandleAreaHeight
+                            self.slideVisible = true
+                            self.closedButton.isHidden = true
+                            self.rightAnchorConstraint?.isActive = false
+                            self.rightAnchorConstraint = self.searchTextField.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -self.sideArchoreConctant)
+                            self.rightAnchorConstraint?.isActive = true
+            }, completion: nil)
+        }
+    }
+    
+
+
+extension SlideMenuView: PresenterHaving {
+    
+    func setPresenter(_ presenter: ViewAstions) {
+        if let presenter = presenter as? SlideMenuViewAction {
+            self.presenter = presenter
+        }
     }
 }
 
