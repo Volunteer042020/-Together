6503814101bfb3b8ceaf9fcbeff8943655c2c739
Tublad <tublad92@gmail.com>
2020-08-09 02:45:37 +0600

    add tableView in ProfileView

---
+++ Together/Screens/Main/ProfileModule/Profile/ProfileTableViewCell/ProfileBetweenLineSectionTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileBetweenLineSectionTableViewCell.swift
+//  Together
+//
+//  Created by Евгений Шварцкопф on 09.08.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class ProfileBetweenLineSectionTableViewCell: UITableViewCell {
+    
+    static var reuseId: String = "BetweenLineSectionTableCell"
+    static var nib: UINib {
+        UINib(nibName: "ProfileBetweenLineSectionTableViewCell", bundle: nil)
+    }
+    
+}
---
+++ Together/Screens/Main/ProfileModule/Profile/ProfileTableViewCell/ProfileEventTapTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileEventTapTableViewCell.swift
+//  Together
+//
+//  Created by Евгений Шварцкопф on 09.08.2020.
+//  Copyright © 2020 GermanyHome. All rights reserved.
+//
+
+import UIKit
+
+class ProfileEventTapTableViewCell: UITableViewCell {
+    
+    static var reuseId: String = "EventTapTableCell"
+    static var nib: UINib {
+        UINib(nibName: "ProfileEventTapTableViewCell", bundle: nil)
+    }
+    
+    @IBOutlet weak var nameEvent: UILabel!
+    
+}
